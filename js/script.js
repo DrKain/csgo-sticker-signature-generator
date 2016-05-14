b4bf303aff04d53ec4d364091f7d9a6828891dc8
The 21st Ranger <bencroft100@outlook.com>
2016-05-15 08:21:26 +1200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -190,12 +190,3 @@ $(document).ready(function() {
     index = index === banners.length - 1 ? 0 : index + 1;
   }, 4000);
 });
-
-
-/* Analytics */
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-76755032-1', 'auto');
-ga('send', 'pageview');
